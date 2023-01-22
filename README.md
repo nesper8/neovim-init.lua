e6a1734fd7a1b8d69926806261639d8275fb555c
nesper8 <naveenp.harivaram@gmail.com>
2023-01-22 23:22:18 +0530

    feat: new init file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # neovim-init.lua
+
+Muh Neovim init file written in pure lua using [kickstart.nvim](https://github.com/nvim-lua/kickstart.nvim/) as a starter
---
+++ lua/settings.lua
@@ -0,0 +1,29 @@
+-- general
+vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.wo.colorcolumn = '100'
+vim.opt.cmdheight = 2
+vim.opt.showtabline = 2
+vim.opt.showmode = false
+
+-- scrolloff
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 6
+
+-- remove whitespace on save
+vim.cmd([[au BufWritePre * :%s/\s\+$//e]])
+
+-- tabbing and indenting
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.expandtab = true
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.bo.copyindent = true
+vim.bo.preserveindent = true
+vim.opt.list = true
+
+-- don't auto commenting new lines
+vim.cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
+
